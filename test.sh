bc92226578f6624deffe0585e7af14d66986404a
Matt Jones <gitcode@magisa.org>
2016-03-25 01:19:01 -0800

    Simple shell script to run the transforms.

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Test the XSLT by running it on al of the ISO docs
+
+TEST="tests/iso"
+TARGET=build
+ISOBASE="iso-0"
+EMLBASE="eml-0"
+
+mkdir $TARGET
+
+for num in 1 2 3 4 5 6 7 8
+do
+    xml tr src/iso2eml.xsl $TEST/$ISOBASE${num}.xml > $TARGET/${EMLBASE}${num}.xml
+done
